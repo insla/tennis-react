90e619c50a691a81e6134992b8551240eadc755a
Slava <shpirkin95@mail.ru>
2022-11-11 19:29:34 +0300

    new slider

--- src/pages/main/coach/Coach.js
+++ src/pages/main/coach/Coach.js
@@ -1,9 +1,8 @@
 import './Coach.scss';
-import Slider from 'react-slick'
-import "slick-carousel/slick/slick.css"; 
-import "slick-carousel/slick/slick-theme.css";
 import useServices from '../../../services/Services';
 import { useState, useEffect } from 'react';
+import { CarouselProvider, Slider, Slide, ButtonBack, ButtonNext, DotGroup } from 'pure-react-carousel';
+import 'pure-react-carousel/dist/react-carousel.es.css';
 import arrowLeft from '../../../resources/icons/arrow_left.png';
 import arrowRight from '../../../resources/icons/arrow_right.png';
 import ErrorMessage from '../../../components/errorMessage/ErrorMessage';
@@ -26,68 +25,25 @@ const Coach = () => {
 
     const renderCoach = () => {
         return (
-            coach.map(item => (
-                <div key={item.id}>
-                    <div className="coach__wrapper">
-                        <div className="coach__img">
-                            <img src={item.img}  alt={item.altImg}/>
-                        </div>
-                        <div className="coach__text">
-                            <p className="coach__name">{item.name}</p>
-                            <p className="coach__description">{item.description}</p>
+            coach.map((item, i) => (
+                <Slide index={i} key={item.id}>
+                    <div className='coach__align-items'>
+                        <div className="coach__wrapper">
+                            <div className="coach__img">
+                                <img src={item.img}  alt={item.altImg}/>
+                            </div>
+                            <div className="coach__text">
+                                <p className="coach__name">{item.name}</p>
+                                <p className="coach__description">{item.description}</p>
+                            </div>
                         </div>
                     </div>
-                </div>
+                </Slide>
             ))
         )
         
     }
 
-    const LeftNavButton = (props) => {
-        const {onClick} = props
-
-        return (
-            <button
-                type="button"
-                className='slick-prev slick-arrow'
-                onClick={onClick}
-            >
-                <img src={arrowLeft} alt="arrow_left"/>
-            </button>
-        );
-    }
-
-    const RightNavButton = (props) => {
-        const {onClick} = props
-
-        return (
-            <button
-                type="button"
-                className="slick-next slick-arrow"
-                onClick={onClick}
-            >
-                <img src={arrowRight} alt="arrow_right"/>
-            </button>
-        );
-    }
-
-    const settings = {
-        slidesToShow: 3,
-        slidesToScroll: 1,
-        dots: true,
-        autoplay: true,
-        autoplaySpeed: 1500,
-        prevArrow: <LeftNavButton/>,
-        nextArrow: <RightNavButton/>,
-        responsive: [{
-            breakpoint: 767,
-            settings: {
-                slidesToShow: 1,
-                slidesToScroll: 1,
-                arrows: false
-            }
-        }]
-    }    
 
     const spinner = loading ? <Spinner/> : null; 
     const errorMessage = error ? <ErrorMessage/> : null;
@@ -97,14 +53,27 @@ const Coach = () => {
         <section className="coach">
             <div className="container">
                 <h2 className="title title_centr">Тренерский состав</h2>
-
                 {spinner}
                 {errorMessage}
-
-                <Slider {...settings}>
-
-                    {items}
-                </Slider>
+                
+                <CarouselProvider
+                    naturalSlideHeight={125} 
+                    naturalSlideWidth={100} 
+                    totalSlides={6} 
+                    isPlaying={true}
+                    interval={2000}
+                    infinite={true} 
+                    lockOnWindowScroll={true}
+                    isIntrinsicHeight={true} 
+                    visibleSlides={3} 
+                    currentSlide={1}>
+                        <Slider>
+                            {items}
+                        </Slider>
+                        <ButtonBack className='coach__button-left'><img src={arrowLeft} alt="arrowLeft" /></ButtonBack>
+                        <ButtonNext className='coach__button-right'><img src={arrowRight} alt="arrowRight" /></ButtonNext>
+                        <DotGroup ></DotGroup>
+                </CarouselProvider>
             </div>
         </section>
     )
